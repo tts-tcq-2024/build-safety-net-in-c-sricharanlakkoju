e6db2fe698d20955466feec621ad66aba907bf0d
sricharanlakkoju <73473045+sricharanlakkoju@users.noreply.github.com>
2024-07-05 14:55:15 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,12 +1,14 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
-#include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define SOUNDEX_LENGTH 4
 
 char getSoundexCode(char c) {
-    c = toupper(c);
+    c = toupper((unsigned char)c);
     switch (c) {
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
@@ -14,27 +16,55 @@ char getSoundexCode(char c) {
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+        default: return '0';
     }
 }
 
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
+bool isAlpha(char c) {
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
 
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-        }
+int generateSoundex(const char *name, char *soundex, size_t soundexSize) {
+    if (name == NULL || soundex == NULL || soundexSize < SOUNDEX_LENGTH + 1) {
+        return -1;
     }
 
-    while (sIndex < 4) {
+    int nameLen = strlen(name);
+    if (nameLen == 0) {
+        return -2;
+    }
+
+    int i = 0;
+    int sIndex = 0;
+
+    while (i < nameLen && !isAlpha(name[i])) {
+        i++;
+    }
+
+    if (i == nameLen) {
+        return -3;
+    }
+
+    soundex[sIndex++] = toupper((unsigned char)name[i++]);
+    char lastCode = '0';
+
+    while (i < nameLen && sIndex < SOUNDEX_LENGTH) {
+        if (isAlpha(name[i])) {
+            char code = getSoundexCode(name[i]);
+            if (code != '0' && code != lastCode) {
+                soundex[sIndex++] = code;
+                lastCode = code;
+            }
+        }
+        i++;
+    }
+
+    while (sIndex < SOUNDEX_LENGTH) {
         soundex[sIndex++] = '0';
     }
 
-    soundex[4] = '\0';
+    soundex[SOUNDEX_LENGTH] = '\0';
+    return 0;
 }
 
-#endif // SOUNDEX_H
+#endif
